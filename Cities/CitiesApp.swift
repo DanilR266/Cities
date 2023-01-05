3d28bd63491af7b750e2fae3a097209679fc616e
DanilR266 <danilwind8@gmail.com>
2023-01-05 17:13:21 +0700

    Initial Commit

---
+++ Cities/CitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesApp.swift
+//  Cities
+//
+//  Created by Данила on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
